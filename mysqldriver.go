bff11b0d7700053c6976212a311843e2fa7cd92e
kamalshkeir <kamalshkeir@yahoo.fr>
2023-04-08 21:02:37 +0200

    fix

--- mysqldriver.go
+++ mysqldriver.go
@@ -1,5 +1,19 @@
 package mysqldriver
 
-func Use() {
-	IsUsed = true
+import (
+	"database/sql/driver"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var Used = false
+var cd = mysql.MySQLDriver{}
+
+func Use() driver.Driver {
+	Used = true
+	return &cd
+}
+
+func GetDriver() driver.Driver {
+	return &cd
 }
